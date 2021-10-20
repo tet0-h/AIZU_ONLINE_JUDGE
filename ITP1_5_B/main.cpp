1e7088c9e46a8de357d60216a4aec6f84b965c8e
tet0-h <tetokon+github2@gmail.com>
2021-10-20 15:41:28 +0900

    ITP1_5_B

---
+++ ITP1_5_B/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int H,W;
+    vector<int> h;
+    vector<int> w;
+    
+    while (1) {
+        cin >> H >> W;
+
+        if(H == 0 && W == 0){
+            break;
+        }
+        h.push_back(H);
+        w.push_back(W);
+    }
+    int count = 0;
+    for(auto itr=h.begin(); itr != h.end(); itr++){
+        for(int t = 0; t < h[count]; t++){
+            for (int i = 0; i < w[count]; i++) {
+                if(t == 0 || t == h[count] - 1){
+                    printf("#");
+                }else{
+                    if(i == 0 || i == w[count] - 1){
+                        printf("#");
+                    }else{
+                        printf(".");
+                    }
+                }
+            }
+            printf("\n");
+        }
+        count++;
+        printf("\n");
+    }
+}
