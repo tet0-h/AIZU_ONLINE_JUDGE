a6346de2a8501a44598c409db18f92d716e62ca1
tet0-h <tetokon+github2@gmail.com>
2021-12-09 11:21:25 +0900

    ITP1_8_D

---
+++ ITP1_8_D/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s,p;
+    cin >> s;
+    cin >> p;
+    
+    s=s+s;
+    if (s.find(p) != string::npos) {
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
